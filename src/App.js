de41a0e2ee7467fd1db23cb3013af2aad5467acb
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-28 13:28:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+// import { HuePicker } from 'react-color';
 
-function App() {
+import * as am5 from "@amcharts/amcharts5";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+
+//chart type
+import * as am5percent from "@amcharts/amcharts5/percent";
+import "./index.css";
+import data from "./data";
+
+const Chart = () => {
+  let rootChart = useRef();
+  let firstRender = useRef(true);
+
+  const [config, setConfig] = useState({
+    strokeColor: "#000000",
+    fillColor: "#00ffff",
+  });
+
+  useEffect(() => {
+    if (firstRender.current) {
+      let root = am5.Root.new("chartID");
+      rootChart.current = root;
+      firstRender.current = false;
+      let pieChart = createPieChart(config);
+      return () => {
+        pieChart.dispose();
+      };
+    } else {
+      let pieChart = createPieChart(config);
+      return () => {
+        pieChart.dispose();
+      };
+    }
+  }, [config]);
+
+  function createPieChart(config) {
+    rootChart.current.setThemes([am5themes_Animated.new(rootChart.current)]);
+
+    let chart = rootChart.current.container.children.push(
+      am5percent.PieChart.new(rootChart.current, {
+        endAngle: 270,
+      })
+    );
+
+    let series = chart.series.push(
+      am5percent.PieSeries.new(rootChart.current, {
+        valueField: "value",
+        categoryField: "category",
+        colorField: "color",
+        endAngle: 270,
+      })
+    );
+
+    series.states.create("hidden", {
+      endAngle: -90,
+    });
+
+    series.data.setAll(data.set1, {
+      // fill: config.fillColor,
+    });
+
+    series.appear(1000, 100);
+
+    return chart;
+  }
+
+  // function selectDataset(set) {
+  //     series.data = data[set];
+  // }
+
+  //  const handleFillColor = (color) => {
+  //   setConfig(prevState => ({ ...prevState, fillColor: color.hex }))
+  //  }
+  const handleSelectData = (color) => {
+    setConfig((prevState) => ({ ...prevState, fillColor: color.hex }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select className="dropdown">
+        <option value="set1">Data Set 1</option>
+        <option value="set2">Data Set 2</option>
+        <option value="set3">Data Set 3</option>
+      </select>
+
+      <div
+        id="chartID"
+        style={{ width: "100%", height: "500px", alignSelf: "end" }}
+      ></div>
     </div>
   );
-}
-
-export default App;
+};
+export default Chart;
