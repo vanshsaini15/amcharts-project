83ea385fb3f67f881ed812b5635b724ae665ede3
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-28 18:07:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,107 +8,95 @@ import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import * as am5percent from "@amcharts/amcharts5/percent";
 import "./index.css";
 import data from "./data";
-
+let chart;
 const Chart = () => {
-    let rootChart = useRef();
-    let firstRender = useRef(true);
+  let rootChart = useRef();
+  let firstRender = useRef(true);
 
-    const[value, setValue] = useState('')
+  const [value, setValue] = useState('set1')
 
-    const [config, setConfig] = useState({
-        fillColor: "#00ffff",
-        
+  const [config, _setConfig] = useState({
+    fillColor: "#00ffff",
+
+  });
+
+  useEffect(() => {
+    if (firstRender.current) {
+      let root = am5.Root.new("chartID");
+      rootChart.current = root;
+      firstRender.current = false;
+      let pieChart = createPieChart(config);
+      return () => {
+        pieChart.dispose();
+      };
+    } else {
+      let pieChart = createPieChart(config);
+      return () => {
+        pieChart.dispose();
+      };
+    }
+  }, [config, value]);
+
+
+
+  function createPieChart(_config) {
+    rootChart.current.setThemes([am5themes_Animated.new(rootChart.current)]);
+
+    chart = rootChart.current.container.children.push(
+      am5percent.PieChart.new(rootChart.current, {
+        endAngle: 270,
+      })
+    );
+
+    let series = chart.series.push(
+      am5percent.PieSeries.new(rootChart.current, {
+        valueField: "value",
+        categoryField: "category",
+        colorField: "color",
+        endAngle: 270,
+      })
+    );
+
+    series.states.create("hidden", {
+      endAngle: -90,
     });
 
-    useEffect(() => {
-        if (firstRender.current) {
-            let root = am5.Root.new("chartID");
-            rootChart.current = root;
-            firstRender.current = false;
-            let pieChart = createPieChart(config);
-            return () => {
-                pieChart.dispose();
-            };
-        } else {
-            let pieChart = createPieChart(config);
-            return () => {
-                pieChart.dispose();
-            };
-        }
-    }, [config]);
-
-
-
-    function createPieChart(config) {
-        rootChart.current.setThemes([am5themes_Animated.new(rootChart.current)]);
-
-        let chart = rootChart.current.container.children.push(
-            am5percent.PieChart.new(rootChart.current, {
-                endAngle: 270,
-            })
-        );
-
-        let series = chart.series.push(
-            am5percent.PieSeries.new(rootChart.current, {
-                valueField: "value",
-                categoryField: "category",
-                colorField: "color",
-                endAngle: 270,
-            })
-        );
-
-        series.states.create("hidden", {
-            endAngle: -90,
-        });
-
-        series.data.setAll(data.set1);
-        console.log( series.data.setAll(data.set1))
-
-        
-
-        series.appear(1000, 100);
-
-
-
-        return chart;
-    }
-
-
-    // function handleChartData(set){
-    //     let data = set
-    //     setChartData()
-
-    // }
-
-    function selectDataset(e) {
-        
-        // .data = data[set];
-       setValue (e.target.value)
-
-        
-    }
+    series.data.setAll(data[value]);
 
 
 
 
+    series.appear(1000, 100);
 
 
 
-    return (
-        <div>
-            <select onchange={selectDataset} className="dropdown">
-                <option value="set1">Data Set 1</option>
-                <option value="set2">Data Set 2</option>
-                <option value="set3">Data Set 3</option>
-            </select>
+    return chart;
+  }
+
+
+  function selectDataset(e) {
+
+    setValue(e.target.value);
+
+  }
 
 
 
-            <div
-                id="chartID"
-                style={{ width: "100%", height: "500px", alignSelf: "end" }}
-            ></div>
-        </div>
-    );
+  return (
+    <div>
+      <select onChange={selectDataset} className="dropdown">
+        <option value="set1">Data Set 1</option>
+        <option value="set2">Data Set 2</option>
+        <option value="set3">Data Set 3</option>
+      </select>
+
+
+
+      <div
+        id="chartID"
+        style={{ width: "100%", height: "500px", alignSelf: "end" }}
+      ></div>
+    </div>
+  );
 };
 export default Chart;
