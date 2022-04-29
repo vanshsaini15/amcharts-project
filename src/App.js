2e168ac47b0d25951514c0417578f00ca07b8b7a
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-29 23:14:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,23 +9,20 @@ import * as am5percent from "@amcharts/amcharts5/percent";
 import "./index.css";
 import data from "./data";
 
-
 let chart;
 const radArr = [25, 50, 75, 100];
-const innerRadArr = [0, 25,50,75,100]
-
+const innerRadArr = [0, 25, 50, 75, 100];
 
 const Chart = () => {
   let rootChart = useRef();
   let firstRender = useRef(true);
 
-  const [value, setValue] = useState('set1')
+  const [value, setValue] = useState("set1");
 
   const [config, setConfig] = useState({
     fillColor: "#00ffff",
     radius: 90,
-    innerRadius : '0',
-
+    innerRadius: "0",
   });
 
   useEffect(() => {
@@ -44,8 +41,6 @@ const Chart = () => {
       };
     }
   }, [config, value]);
-
-
 
   function createPieChart(config) {
     rootChart.current.setThemes([am5themes_Animated.new(rootChart.current)]);
@@ -74,65 +69,91 @@ const Chart = () => {
     series.setAll({
       radius: am5.percent(config.radius),
       innerRadius: am5.percent(config.innerRadius),
-      
     });
 
-
-
-
-
     // series.appear(1000, 100);
-
-
 
     return chart;
   }
 
-
   function selectDataset(e) {
-
     setValue(e.target.value);
-
   }
-
-
 
   return (
     <div>
       <div className="dropdown d-flex flex-column align-items-center">
-      <select onChange={selectDataset}className="btn btn-secondary dropdown-toggle mt-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-      <option value="set1">Data Set 1</option>
-      <option value="set2">Data Set 2</option>
-      <option value="set3">Data Set 3</option>
-    </select>
-      
-      
+        <select
+          onChange={selectDataset}
+          className="btn btn-secondary dropdown-toggle mt-2"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <option value="set1">Data Set 1</option>
+          <option value="set2">Data Set 2</option>
+          <option value="set3">Data Set 3</option>
+        </select>
       </div>
-     
 
-      
       <div className="dropdown d-flex flex-column align-items-center">
-      <button className="btn btn-secondary dropdown-toggle mt-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          Radius: {config.radius ? config.radius : ''}
-      </button>
-      <ul className="dropdown-menu overflow-auto" aria-labelledby="dropdownMenuButton1" style={{ height: 'auto' }}>
-          {radArr.map((r, i) => <li key={i}><button className="dropdown-item" onClick={() => setConfig(prevState => ({ ...prevState, radius: r }))}>{r}</button></li>)}
-      </ul>
+        <button
+          className="btn btn-secondary dropdown-toggle mt-2"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Radius: {config.radius ? config.radius : ""}
+        </button>
+        <ul
+          className="dropdown-menu overflow-auto"
+          aria-labelledby="dropdownMenuButton1"
+          style={{ height: "auto" }}
+        >
+          {radArr.map((rad, idx) => (
+            <li key={idx}>
+              <button
+                className="dropdown-item"
+                onClick={() =>
+                  setConfig((prevState) => ({ ...prevState, radius: rad }))
+                }
+              >
+                {rad}
+              </button>
+            </li>
+          ))}
+        </ul>
 
-      
-      <button className="btn btn-secondary dropdown-toggle mt-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          Inner Radius: {config.innerRadius ? config.innerRadius : ''}
-      </button>
-      <ul className="dropdown-menu overflow-auto" aria-labelledby="dropdownMenuButton1" style={{ height: 'auto' }}>
-          {innerRadArr.map((r, i) => <li key={i}><button className="dropdown-item" onClick={() => setConfig(prevState => ({ ...prevState, innerRadius: r }))}>{r}</button></li>)}
-      </ul>
-      
-      
-  </div>
-
-
-
-
+        <button
+          className="btn btn-secondary dropdown-toggle mt-2"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Inner Radius: {config.innerRadius ? config.innerRadius : ""}
+        </button>
+        <ul
+          className="dropdown-menu overflow-auto"
+          aria-labelledby="dropdownMenuButton1"
+          style={{ height: "auto" }}
+        >
+          {innerRadArr.map((rad, idx) => (
+            <li key={idx}>
+              <button
+                className="dropdown-item"
+                onClick={() =>
+                  setConfig((prevState) => ({ ...prevState, innerRadius: rad }))
+                }
+              >
+                {rad}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div
         id="chartID"
